bf098b8813d509c87e82cc00a30dbdef9cc92e62
TCP-ENG <124282079+TCP-ENG@users.noreply.github.com>
2024-01-25 16:03:24 -0500

    Initial Code Commit

--- drv814X.cpp
+++ drv814X.cpp
@@ -1,2 +1,244 @@
-#include "Arduino.h"
+#include <Arduino.h>
+#include <SPI.h>
 #include "drv814x.h"
+
+_stat_Byte_ DRV814X::readReg(uint8_t reg)
+{
+
+    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    _command_byte_ commandByte;
+    _stat_Byte_ statByte;
+    commandByte.command_Byte.address=reg;
+    commandByte.command_Byte.readWrite=0x01;
+    commandByte.command_Byte.data=0x00;
+    SerialUSB.println(commandByte.d16);
+    digitalWrite(cs_pin, LOW);
+    statByte.d16 = SPI.transfer16(commandByte.d16);
+    digitalWrite(cs_pin,HIGH);
+    SPI.endTransaction();
+    return statByte;
+}
+_stat_Byte_ DRV814X::writeReg(uint8_t reg,uint8_t val)
+{
+    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    _command_byte_ commandByte;
+    _stat_Byte_ statByte;
+    commandByte.command_Byte.address=reg;
+    commandByte.command_Byte.readWrite=0x01;
+    commandByte.command_Byte.data=val;
+    digitalWrite(cs_pin, LOW);
+    statByte.d16 = SPI.transfer16(commandByte.d16);
+    digitalWrite(cs_pin,HIGH);
+    SPI.endTransaction();
+    return statByte;
+}
+_device_ID_ DRV814X::getID(void)
+{
+    _stat_Byte_ temp = readReg(DEVICE_ID);
+    _device_ID_ dID;
+    dID.d8 = temp.stat_Byte.DATA;
+    return dID;
+}
+
+_fault_sum_ DRV814X::getFaultSum(void)
+{
+    _stat_Byte_ temp = readReg(FAULT_SUMMARY);
+    _fault_sum_ fSum;
+    fSum.d8 = temp.stat_Byte.DATA;
+    return fSum;
+}
+
+_status1_ DRV814X::getStatus1(void)
+{
+    _stat_Byte_ temp = readReg(STATUS1);
+    _status1_ stat;
+    stat.d8 = temp.stat_Byte.DATA;
+    return stat;
+}
+
+_status2_ DRV814X::getStatus2(void)
+{
+    _stat_Byte_ temp = readReg(STATUS2);
+    _status2_ stat;
+    stat.d8 = temp.stat_Byte.DATA;
+    return stat;
+}
+
+_command_ DRV814X::getCommand(void)
+{
+    _stat_Byte_ temp = readReg(COMMAND);
+    _command_ cmd;
+    cmd.d8 = temp.stat_Byte.DATA;
+    return cmd;
+}
+
+void DRV814X::setCommand(_command_ cmd)
+{
+    writeReg(COMMAND, cmd.d8);
+}
+
+_spi_in_ DRV814X::getSPIin(void)
+{
+    _stat_Byte_ temp = readReg(SPI_IN);
+    _spi_in_ spi_in;
+    spi_in.d8 = temp.stat_Byte.DATA;
+    return spi_in;
+}
+
+void DRV814X::setSPIin(_spi_in_ sIn)
+{
+    writeReg(SPI_IN, sIn.d8);
+}
+
+_config1_ DRV814X::getConfig1(void)
+{
+    _stat_Byte_ temp = readReg(CONFIG1);
+    _config1_ cfg;
+    cfg.d8 = temp.stat_Byte.DATA;
+    return cfg;
+}
+
+void DRV814X::setConfig1(_config1_ cfg)
+{
+    writeReg(CONFIG1, cfg.d8);
+}
+
+_config2_ DRV814X::getConfig2(void)
+{
+    _stat_Byte_ temp = readReg(CONFIG2);
+    _config2_ cfg;
+    cfg.d8 = temp.stat_Byte.DATA;
+    return cfg;
+}
+
+void DRV814X::setConfig2(_config2_ cfg)
+{
+    writeReg(CONFIG2, cfg.d8);
+}
+
+_config3_ DRV814X::getConfig3(void)
+{
+    _stat_Byte_ temp = readReg(CONFIG3);
+    _config3_ cfg;
+    cfg.d8 = temp.stat_Byte.DATA;
+    return cfg;
+}
+
+void DRV814X::setConfig3(_config3_ cfg)
+{
+    writeReg(CONFIG3, cfg.d8);
+}
+
+_config4_ DRV814X::getConfig4(void)
+{
+    _stat_Byte_ temp = readReg(CONFIG4);
+    _config4_ cfg;
+    cfg.d8 = temp.stat_Byte.DATA;
+    return cfg;
+}
+
+void DRV814X::setConfig4(_config4_ cfg)
+{
+    writeReg(CONFIG4, cfg.d8);
+}
+
+void DRV814X::setPWM(uint8_t duty)
+{
+    analogWrite(drive_pin,duty);
+}
+uint16_t DRV814X::getAIPROP(void)
+{
+    _device_ID_ temp = getID();
+    return getAIPROP(temp.d8);
+}
+uint16_t DRV814X::getAIPROP(uint8_t dID)
+{
+    switch (dID)
+    {
+    case DRV8143S:
+        return 3050;
+        break;
+    case DRV8144S:
+        return 4630;
+        break;
+    case DRV8145S:
+        return 6230;
+        break;
+    case DRV8143P:
+        return 3040;
+        break;
+    case DRV8145P:
+        return 5980;
+        break;
+    
+    default:
+        break;
+    }
+}
+float DRV814X::current(float volts)
+{
+    float IPROPI = volts/RIPROP;
+    return IPROPI * AIPROP;
+}
+void DRV814X::setS_ITRIP(uint8_t s_itrip)
+{
+    _config2_ cfg;
+    cfg.config2.S_ITRIP = s_itrip;
+    writeReg(CONFIG2,cfg.d8);
+}
+void DRV814X::setS_ITRIP(float cur, uint8_t hiLow)
+{
+    float ITrip[8];
+    for(int x = 1; x < 8; x++)
+    {
+        ITrip[x]=current(VTrip[x]);
+        if(ITrip[x] >= cur)
+        {
+            if(hiLow == 1)
+            {
+                setS_ITRIP(x);
+                x = 9;
+            }
+            else
+            {
+                x--;
+                if(x<1)
+                {
+                    x=1;
+                }
+                setS_ITRIP(x);
+                x = 9;
+            }
+        }
+    }
+}
+
+DRV814X::DRV814X()
+{
+
+}
+
+DRV814X::DRV814X(uint8_t dpin, uint8_t cspin, int rp)
+{
+    drive_pin = dpin;
+    cs_pin = cspin;
+    RIPROP = rp;
+    AIPROP = getAIPROP();
+}
+
+DRV814X::~DRV814X()
+{
+}
+
+void DRV814X::setDrivePin(uint8_t dPin)
+{
+    drive_pin = dPin;
+}
+void DRV814X::setCSpin(uint8_t csPin)
+{
+    cs_pin=csPin;
+}
+void DRV814X::setRIProp(int riprop)
+{
+    RIPROP=riprop;
+}
--- drv814x.h
+++ drv814x.h
@@ -1,7 +1,217 @@
 #ifndef _DRV814X_H_
 #define _DRV814X_H_
 
-#include "Aruduino.h"
+#include "Arduino.h"
+
+#define DEVICE_ID 0x00
+#define FAULT_SUMMARY 0x01
+#define STATUS1 0x02
+#define STATUS2 0x03
+#define COMMAND 0x08
+#define SPI_IN  0x09
+#define CONFIG1 0x0A
+#define CONFIG2 0x0B
+#define CONFIG3 0x0C
+#define CONFIG4 0x0D
+
+#define DRV8143S    0xBA
+#define DRV8144S    0xCA
+#define DRV8145S    0xDA
+#define DRV8143P    0xBE
+#define DRV8145P    0xDE
+
+
+typedef union _command_byte_
+{
+    uint16_t d16;
+    struct 
+    {
+        uint8_t data:8;
+        uint8_t address:6;
+        uint8_t readWrite:1;
+        uint8_t NOT_USED:1;
+        
+    }command_Byte;
+};
+typedef union _stat_Byte_
+{
+    uint16_t d16; 
+    struct 
+    {
+        uint8_t DATA:8;
+        uint8_t SPI_E_RR:1;
+        uint8_t TSD:1;
+        uint8_t OCP:1;
+        uint8_t VMUV:1;
+        uint8_t VMOV:1;
+        uint8_t FAULT:1;
+        uint8_t NOT_USED:2;
+    }stat_Byte;
+};
+typedef union _device_ID_
+{
+    uint8_t d8;
+    struct 
+    {
+        uint8_t REV_ID:2;
+        uint8_t DEV_ID:6;
+    }device_ID;
+};
+typedef union _fault_sum_
+{
+    uint8_t d8;
+    struct
+    {
+        uint8_t VCPUV:1;
+        uint8_t TSD:1;
+        uint8_t OCP:1;
+        uint8_t VMUV:1;
+        uint8_t VMOV:1;
+        uint8_t FAULT:1;
+        uint8_t POR:1;
+        uint8_t SPI_ERR:1;
+    }fault_sum;   
+};
+typedef union _status1_
+{
+    uint8_t d8;
+    struct
+    {
+        uint8_t OCP_L_2:1;
+        uint8_t OCP_H_2:1;
+        uint8_t OCP_L:1;
+        uint8_t OCP_H:1;
+        uint8_t ACTIVE:1;
+        uint8_t ITRIP_CMP:1;
+        uint8_t OLA:2;
+    }status1;
+};
+typedef union _status2_
+{    
+    uint8_t d8;
+    struct
+    {
+        uint8_t OLP_CMP:1;
+        uint8_t NOT_USED2:3;
+        uint8_t ACTIVE:1;
+        uint8_t NOT_USED:2;
+        uint8_t DRVOFF_STAT:1;
+    }status2;
+};
+typedef union _command_
+{
+    uint8_t d8;
+    struct 
+    {
+        uint8_t REG_LOCK:2;
+        uint8_t NOT_USED2:1;
+        uint8_t SPI_IN_LOCK:2;
+        uint8_t NOT_USED:2;
+        uint8_t CLR_FLT:1;
+    }command;
+};
+typedef union _spi_in_
+{
+    uint8_t d8;
+    struct 
+    {
+        uint8_t S_IN:1;
+        uint8_t SPARE:2;
+        uint8_t S_DRVOFF:1;
+        uint8_t NOT_USED:4;
+    }spi_in;  
+};
+typedef union _config1_
+{
+    uint8_t d8;
+    struct 
+    {
+        uint8_t OLA_RETRY:1;
+        uint8_t VMOV_RETRY:1;
+        uint8_t TSD_RETRY:1;
+        uint8_t OCP_RETRY:1;
+        uint8_t SSC_DIS:1;
+        uint8_t VMOV_SEL:2;
+        uint8_t EN_OLA:1;
+    }config1;  
+};
+typedef union _config2_
+{
+    uint8_t d8;
+    struct 
+    {
+        uint8_t S_ITRIP:3;
+        uint8_t NOT_USED:2;
+        uint8_t S_DIAG:2;
+        uint8_t SPARE:1;
+    }config2;
+};
+typedef union _config3_
+{
+    uint8_t d8;
+    struct
+    {
+        uint8_t SPARE:2;
+        uint8_t S_SR:3;
+        uint8_t NOT_USED:1;
+        uint8_t TOFF:2;
+    }config3;
+};
+typedef union _config4_
+{
+    uint8_t d8;
+    struct
+    {
+        uint8_t IN_SEL:1;
+        uint8_t SPARE:1;
+        uint8_t DRVOFF_SEL:1;
+        uint8_t OCP_SEL:2;
+        uint8_t NOT_USED:1;
+        uint8_t TOCP_SEL:2;
+    }config4;
+};   
+class DRV814X
+{
+private:
+    /* data */
+    uint8_t drive_pin;
+    uint8_t cs_pin;
+    int RIPROP;
+    uint32_t AIPROP;
+    float VTrip[8] = {0,1.18,1.41,1.65,1.98,2.31,2.64,2.97};
+    _stat_Byte_ readReg(uint8_t reg);
+    _stat_Byte_ writeReg(uint8_t reg, uint8_t value);
+public:
+    DRV814X(uint8_t dpin, uint8_t cspin,int rp);
+    DRV814X();
+    ~DRV814X();
+    _device_ID_ getID(void);
+    _fault_sum_ getFaultSum(void);
+    _status1_ getStatus1(void);
+    _status2_ getStatus2(void);
+    _command_ getCommand(void);
+    void setCommand(_command_ cmd);
+    _spi_in_ getSPIin(void);
+    void setSPIin(_spi_in_ spiin);
+    _config1_ getConfig1(void);
+    void setConfig1(_config1_ cfg1);
+    _config2_ getConfig2(void);
+    void setConfig2(_config2_ cfg2);
+    _config3_ getConfig3(void);
+    void setConfig3(_config3_ cfg3);
+    _config4_ getConfig4(void);
+    void setConfig4(_config4_ cfg4);
+    void setPWM(uint8_t duty);
+    float current(float volts);
+    uint16_t getAIPROP(void);
+    uint16_t getAIPROP(uint8_t dID);
+    void setS_ITRIP(uint8_t s_itrip);
+    void setS_ITRIP(float cur, uint8_t hiLow);
+    void setDrivePin(uint8_t dPin);
+    void setCSpin(uint8_t csPin);
+    void setRIProp(int riprop);
+
+};
 
 
 
